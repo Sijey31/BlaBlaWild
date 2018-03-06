6914d36fc7435371f5448cb5a03edb053a61db5a
Thomas Patérac <paterac.thomas@gmail.com>
2018-03-06 15:57:22 +0100

    init commit

---
+++ app/src/main/java/com/example/wilder/blablawild/TripAdapter.java
@@ -0,0 +1,10 @@
+package com.example.wilder.blablawild;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by wilder on 06/03/18.
+ */
+
+public class TripAdapter extends ArrayAdapter {
+}
---
+++ app/src/main/java/com/example/wilder/blablawild/TripModel.java
@@ -0,0 +1,8 @@
+package com.example.wilder.blablawild;
+
+/**
+ * Created by wilder on 06/03/18.
+ */
+
+public class TripModel {
+}
