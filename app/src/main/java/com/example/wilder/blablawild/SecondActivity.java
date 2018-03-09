2acda90c4502de904d0a78165caa4999508622bf
Thomas Patérac <paterac.thomas@gmail.com>
2018-03-09 13:43:48 +0100

    ajout datepicker

--- app/src/main/java/com/example/wilder/blablawild/SecondActivity.java
+++ app/src/main/java/com/example/wilder/blablawild/SecondActivity.java
@@ -1,49 +1,96 @@
 package com.example.wilder.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class SecondActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        final EditText editdate = findViewById(R.id.edit_date);
+        final EditText editdeparture = findViewById(R.id.edit_departure);
+        final EditText editname = findViewById(R.id.edit_name);
 
         Button buttonsearch = findViewById(R.id.button_search);
-        buttonsearch.setOnClickListener(new View.OnClickListener() {
+        buttonsearch.setOnClickListener(new View.OnClickListener()
+        {
+
             @Override
-            public void onClick(View view) {
-                EditText editdeparture = findViewById(R.id.edit_departure);
+            public void onClick(View view)
+            {
+
                 String departure = editdeparture.getText().toString();
-                EditText editname = findViewById(R.id.edit_name);
                 String name = editname.getText().toString();
-                EditText editdate = findViewById(R.id.edit_date);
-                String date = editdate.getText().toString();
 
                 if
                         (departure.equals("") || (name.equals("")))
                 {
-                    Toast.makeText(SecondActivity.this, "Please fill your departure and destination!",Toast.LENGTH_LONG).show();
+                    Toast.makeText(SecondActivity.this, "Please fill your departure and destination!", Toast.LENGTH_LONG).show();
                 }
-
                 else
                 {
-                    Intent intent = new Intent(SecondActivity.this,ItineraryListActivity.class);
-                    intent.putExtra("extradeparture",departure);
-                    intent.putExtra("extraname",name);
+                    Intent intent = new Intent(SecondActivity.this, ItineraryListActivity.class);
+                    intent.putExtra("extradeparture", departure);
+                    intent.putExtra("extraname", name);
                     startActivity(intent);
                 }
 
 
+
             }
         });
+
+        final Calendar mycalendar = Calendar.getInstance();
+        final DatePickerDialog.OnDateSetListener datePicker = new DatePickerDialog.OnDateSetListener()
+
+        {
+
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+
+            {
+                mycalendar.set(Calendar.YEAR, year);
+                mycalendar.set(Calendar.MONTH, month);
+                mycalendar.set(Calendar.DAY_OF_MONTH, day);
+                updateLabel(editdate,mycalendar);
+            }
+        };
+
+        editdate.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view)
+
+            {
+                new DatePickerDialog(SecondActivity.this, datePicker, mycalendar.get(Calendar.YEAR),
+                        mycalendar.get(Calendar.MONTH),mycalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+
+        });
+    }
+
+    private void updateLabel(EditText edittext, Calendar mycalendar)
+
+    {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.FRANCE);
+        edittext.setText(sdf.format(mycalendar.getTime()));
     }
 }
 
--- app/src/main/java/com/example/wilder/blablawild/TripAdapter.java
+++ app/src/main/java/com/example/wilder/blablawild/TripAdapter.java
@@ -1,10 +1,51 @@
 package com.example.wilder.blablawild;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 /**
  * Created by wilder on 06/03/18.
  */
+public class TripAdapter extends ArrayAdapter<TripModel>
+{
 
-public class TripAdapter extends ArrayAdapter {
+    public TripAdapter(Context context, ArrayList<TripModel> trips)
+    {
+        super(context, 0, trips);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        // Get the data item for this position
+        TripModel trip = getItem(position);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_trip, parent, false);
+        }
+        // Lookup view for data population
+        TextView firstName = (TextView) convertView.findViewById(R.id.text_firstname);
+        TextView lastName = (TextView) convertView.findViewById(R.id.text_lastname);
+        TextView textDate = (TextView) convertView.findViewById(R.id.text_date);
+        TextView textPrice = (TextView) convertView.findViewById(R.id.text_price);
+        // Populate the data into the template view using the data object
+        firstName.setText(trip.getFirstname());
+        lastName.setText(trip.getLastname());
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+        String dateformat = sdf.format(trip.getDate());
+        String date = dateformat + "PM";
+        textDate.setText(date);
+        String price = "$" +String.valueOf(trip.getPrice());
+        textPrice.setText(price);
+
+
+        return convertView;
+    }
 }
